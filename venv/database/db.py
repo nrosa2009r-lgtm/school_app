5958e1cc7ae679cc2e6372c38267843c3597fb4e
Nrpl <n.rosa2009r@gmail.com>
2026-06-24 22:28:07 +0200

    Added readme
    Added a README file with project details and features.

--- venv/database/db.py
+++ venv/database/db.py
@@ -1,10 +1,10 @@
 import asyncio
 from sqlalchemy import ForeignKey, String, select
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from config.get_variable import db_url
 
-# Inicjalizacja asynchronicznego silnika i fabryki sesji
+# Create engine
 engine = create_async_engine(db_url, echo=True)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
@@ -18,10 +18,19 @@ class Users(Base):
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     last_name: Mapped[str] = mapped_column(String(50), nullable=False)
     email: Mapped[str] = mapped_column(String(50), nullable=False)
-    role: Mapped[str] = mapped_column(String(10), nullable=False)
+    role: Mapped[str] = mapped_column(String(10), nullable=False)   #rootadmin/admin/teacher/student
     password: Mapped[str] = mapped_column(String(256), nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=False)
+    u_class: Mapped[str] = mapped_column(String(10),nullable=True)
 
 # Funkcja tworząca tabele w bazie danych
 async def create_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+
+async def is_root_admin_exist():
+    async with async_session() as session:
+        stmt = select(Users).where(Users.role == "root_admin")
+        result = await session.execute(stmt)
+        return result.scalar_one_or_none()
--- venv/main.py
+++ venv/main.py
@@ -1,10 +1,13 @@
 import asyncio
 from database.db import create_db
-
+from services.create_admin import create_root_admin
 async def main():
     print("Rozpoczynam tworzenie bazy danych...")
     await create_db()
     print("Baza danych i tabele zostały pomyślnie utworzone!")
+    await create_root_admin("natan","rosa","nrpl350@gmail.com","Trawis8!")
+
+
 
 if __name__ == "__main__":
     asyncio.run(main())
